9251965c0b92dd44d20b209a2e7c0e7cc7a84077
Muchen He <i@muchen.ca>
2018-09-22 00:05:47 -0600

    Added instant concrete powder recipe

--- src/InstantConcrete.py
+++ src/InstantConcrete.py
@@ -3,23 +3,28 @@ import os
 import shutil
 
 from common.PackData import *
+from common.Recipe import *
 
 NAME = 'InstantConcrete'
-VERS = '0.0'
+VERS = '1.0'
 
 CWD = os.getcwd()
 
 def cleanup():
 	path = os.path.join(CWD, 'dist', NAME)
 	try:
-		os.rmdir(path)
-	except OSError:
+		shutil.rmtree(path)
+	except Exception:
 		print('Clean up of %s failed' % path)
 	else:
 		print('Cleaned up %s' % path)
 
+def makeRecipe(recipePath, recipeName, recipe):
+	with open(os.path.join(recipePath, recipeName + '.json'), 'w', encoding='utf-8') as outfile:
+		json.dump(recipe.getSerialized(), outfile, ensure_ascii=False)
+
 def build():
-	# create directories
+	# List directories
 	dirList = []
 
 	path = os.path.join(CWD, 'dist', NAME)
@@ -29,10 +34,50 @@ def build():
 	recipesDir = os.path.join(path, 'data', namespace, 'recipes')
 	dirList.append(recipesDir)
 
+	# Create dirs
+	for dirs in dirList:
+		os.makedirs(dirs)
+
 	# Create pack metadata
 	with open(os.path.join(path, 'pack.mcmeta'), 'w', encoding='utf-8') as outfile:
 		metadata = getPackData(NAME, VERS)
 		json.dump(metadata, outfile, ensure_ascii=False)
+
+	# Recipe constants
+	TYPES = [
+		'minecraft:white_concrete',
+		'minecraft:orange_concrete',
+		'minecraft:magenta_concrete',
+		'minecraft:light_blue_concrete',
+		'minecraft:yellow_concrete',
+		'minecraft:lime_concrete',
+		'minecraft:pink_concrete',
+		'minecraft:gray_concrete',
+		'minecraft:light_gray_concrete',
+		'minecraft:cyan_concrete',
+		'minecraft:purple_concrete',
+		'minecraft:blue_concrete',
+		'minecraft:brown_concrete',
+		'minecraft:green_concrete',
+		'minecraft:red_concrete'
+	]
+	PRE_CRAFT_SUFFIX = '_powder'
+	PATTERN = [
+		'CC',
+		'CC'
+	]
+
+	# For each variant make recipe
+	for block in TYPES:
+		newRecipe = ShapedRecipe()
+		newRecipe.definePattern(PATTERN)
+		newRecipe.defineKey({
+			'C': block + PRE_CRAFT_SUFFIX
+		})
+		newRecipe.setResult(block, 4)
+
+		itemName = block.split(':')[-1]
+		makeRecipe(recipesDir, itemName, newRecipe)
 
 cleanup()
 build()
---
+++ src/common/Recipe.py
@@ -0,0 +1,67 @@
+class ShapedRecipe:
+	def __init__(self):
+		self.type = 'crafting_shaped'
+
+	def definePattern(self, grid):
+		# check the pattern is valid
+		gridHeight = len(grid)
+		if gridHeight not in [2, 3]:
+			raise RuntimeError('Recipe pattern is not the correct size')
+
+		for row in grid:
+			rowWidth = len(row)
+			if rowWidth != len(grid[0]):
+				raise RuntimeError('Recipe pattern is not the correct size')
+
+		# Set to pattern
+		self.pattern = grid
+
+	def defineKey(self, mapping):
+		if not hasattr(self, 'pattern'):
+			raise RuntimeError('Recipe pattern does not exist')
+
+
+		# Check that the key exists in pattern
+		for key in mapping:
+			if not self.isKeyInPattern(key):
+				print('WARNING:', key, 'is not in recipe pattern')
+		
+		# Check that the pattern has keys
+		checked = ''
+		for row in self.pattern:
+			for cell in row:
+				if cell not in checked:
+					if cell not in mapping:
+						raise RuntimeError('Item in recipe pattern does not have mapping')
+					else:
+						checked += cell
+
+		# Construct serializable obj
+		self.key = {}
+		for key in mapping:
+			self.key[key] = {
+				'item': mapping[key]
+			}
+
+	def isKeyInPattern(self, key):
+		for row in self.pattern:
+			if key in row:
+				return True
+
+		return False
+
+	def setResult(self, result, count):
+		self.result = {
+			'item': result,
+			'count': count
+		}
+
+	def getSerialized(self):
+		data = {
+			'type': self.type,
+			'pattern': self.pattern,
+			'key': self.key,
+			'result': self.result
+		}
+
+		return data
