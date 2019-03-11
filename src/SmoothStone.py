25f37a1c9ccd66571e9c1b445dc1c6fe463905fb
Muchen He <mansurhe@outlook.com>
2019-03-10 21:34:18 -0700

    Added smooth stone recipe

---
+++ src/SmoothStone.py
@@ -0,0 +1,55 @@
+import json
+import os
+import shutil
+
+from common.PackData import *
+from common.Recipe import *
+
+NAME = 'SmoothStone'
+VERS = '1.0'
+
+CWD = os.getcwd()
+
+def cleanup():
+	path = os.path.join(CWD, 'dist', NAME)
+	try:
+		shutil.rmtree(path)
+	except Exception:
+		print('Clean up of %s failed' % path)
+	else:
+		print('Cleaned up %s' % path)
+
+
+def makeRecipe(recipePath, recipeName, recipe):
+	with open(os.path.join(recipePath, recipeName + '.json'), 'w', encoding='utf-8') as outfile:
+		json.dump(recipe.getSerialized(), outfile, ensure_ascii=False)
+
+
+def build():
+	# List directories
+	dirList = []
+
+	path = os.path.join(CWD, 'dist', NAME)
+	dirList.append(path)
+
+	namespace = 'mdp_sandstone_to_sand'
+	recipesDir = os.path.join(path, 'data', namespace, 'recipes')
+	dirList.append(recipesDir)
+
+	# Create dirs
+	for dirs in dirList:
+		os.makedirs(dirs)
+
+	# Create pack metadata
+	with open(os.path.join(path, 'pack.mcmeta'), 'w', encoding='utf-8') as outfile:
+		metadata = getPackData(NAME, VERS)
+		json.dump(metadata, outfile, ensure_ascii=False)
+
+	# New recipe
+	newRecipe = ShapelessRecipe()
+	newRecipe.setIngredients(['minecraft:stone_slab', 'minecraft:stone_slab'])
+	newRecipe.setResult('minecraft:smooth_stone', 1)
+	makeRecipe(recipesDir, 'smooth_stone', newRecipe)
+
+cleanup()
+build()
